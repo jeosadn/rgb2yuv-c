ac23ce886a6e2c730306b9db39f18716fde9d07f
Boris94cr <baach94@hotmail.com>
2019-06-24 01:06:50 -0600

    Adding time measures and format changes

--- rgb2yuv-c.c
+++ rgb2yuv-c.c
@@ -5,10 +5,74 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 using namespace std;
 
-char* convert(char buf[3]) {
+int menu(int argc, char **argv, char **RGB, char **YUV){
+    //char *RGB = NULL;
+    //char *YUV = NULL;
+    int authors = 0;
+    int help = 0;
+
+    int opt;
+
+    while ((opt = getopt(argc, argv, "i:o:ha")) != -1) {
+    
+        switch (opt) {
+	    case 'h':
+                help = 1;
+        	break;
+       	    case 'a':
+    		authors = 1;
+                break;
+	    case 'i':
+	        *RGB = optarg;
+		break;
+	    case 'o':
+		*YUV = optarg;
+		break;
+       	    case '?':
+   	    	printf("unknown option: %c\n", optopt);
+   	    	break;
+        }
+    }
+
+    printf("         _____   _____ ____     _         __     ___    ___      __ \n ");
+    printf("	|  __ \\ / ____|  _ \\   | |        \\ \\   / / |  | \\ \\    / / \n ");
+    printf("	| |__) | |  __| |_) |  | |_ ___    \\ \\_/ /| |  | |\\ \\  / /  \n ");
+    printf("	|  _  /| | |_ |  _ <   | __/ _ \\    \\   / | |  | | \\ \\/ /   \n ");
+    printf("	| | \\ \\| |__| | |_) |  | || (_) |    | |  | |__| |  \\  /    \n ");
+    printf("	|_|  \\_\\\\_____|____/    \\__\\___/     |_|   \\____/    \\/     \n\n\n" );
+
+    if (help == 0) {
+        if (authors) {
+	    printf("Authors: Boris Altamirano - Daniel Jimenez - Jose Andres Pacheco\n\n");
+	} else {
+	    if (*RGB != NULL) {
+	        printf("Using RGB File: %s.\n",*RGB);
+	    } else {
+		printf("No RGB File specified.\n\n");
+		return 1;
+	    }
+
+	    if (*YUV != NULL) {
+	        printf("Writing output YUV file name as: %s.\n\n",*YUV);
+	    } else {
+		printf("No output YUV File name specified.\n\n");
+		return 1;
+	    }
+	}
+    } else {
+   	printf("RGB to YUV images files converter.\n\n");
+	printf("Options: \n\t -a \t\t Displays authors names. \n\t -h \t\t Displays this menu. \n");
+	printf("\t -i 'RGBFile' \t Specifies the RGB file to be converted. \n\t -o 'YUVFile' \t Specifies the output YUV file name.\n\n");
+    }
+    return 0;
+   	
+}
+
+char* convert_pixel(char buf[3]) {
 
     float buf2[3];
     float result_float[3];	
@@ -40,102 +104,56 @@ char* convert(char buf[3]) {
     return result_pixel;
 } 
 
-int main (int argc, char **argv) {
+void rgb2yuv (char *input_image, char *output_image){
+    ifstream in_image;
+    ofstream out_image;
 
-    //----------------------//	
-    //    Executing menu    //
-    //----------------------//
-
-    char *RGB = NULL;
-    char *YUV = NULL;
-    int authors = 0;
-    int help = 0;
-
-    int opt;
-
-    while ((opt = getopt(argc, argv, "i:o:ha")) != -1) {
-    
-        switch (opt) {
-	    case 'h':
-                help = 1;
-        	break;
-       	    case 'a':
-    		authors = 1;
-                break;
-	    case 'i':
-	        RGB = optarg;
-		break;
-	    case 'o':
-		YUV = optarg;
-		break;
-       	    case '?':
-   	    	printf("unknown option: %c\n", optopt);
-   	    	break;
-        }
-    }
-
-    printf("         _____   _____ ____     _         __     ___    ___      __ \n ");
-    printf("	|  __ \\ / ____|  _ \\   | |        \\ \\   / / |  | \\ \\    / / \n ");
-    printf("	| |__) | |  __| |_) |  | |_ ___    \\ \\_/ /| |  | |\\ \\  / /  \n ");
-    printf("	|  _  /| | |_ |  _ <   | __/ _ \\    \\   / | |  | | \\ \\/ /   \n ");
-    printf("	| | \\ \\| |__| | |_) |  | || (_) |    | |  | |__| |  \\  /    \n ");
-    printf("	|_|  \\_\\\\_____|____/    \\__\\___/     |_|   \\____/    \\/     \n\n\n" );
-
-    if (help == 0) {
-        if (authors) {
-	    printf("Authors: Boris Altamirano - Daniel Jimenez - Jose Andres Pacheco\n\n");
-	} else {
-	    if (RGB != NULL) {
-	        printf("Using RGB File: %s.\n",RGB);
-	    } else {
-		printf("No RGB File specified.\n\n");
-		return 0;
-	    }
-
-	    if (YUV != NULL) {
-	        printf("Writing output YUV file name as: %s.\n\n",YUV);
-	    } else {
-		printf("No output YUV File name specified.\n\n");
-		return 0;
-	    }
-	}
-    } else {
-   	printf("RGB to YUV images files converter.\n\n");
-	printf("Options: \n\t -a \t\t Displays authors names. \n\t -h \t\t Displays this menu. \n");
-	printf("\t -i 'RGBFile' \t Specifies the RGB file to be converted. \n\t -o 'YUVFile' \t Specifies the output YUV file name.\n\n");
-    }
-
-    //----------------------//	
-    // Executing convertion //
-    //----------------------//
-
-    ifstream image;
-    ofstream output_image;
-
-    char pixels_rgb[640*480*3];
+    //char pixels_rgb[640*480*3];
     char pixels_yuv[640*480*3];
 
-    printf("Executing convertion!!\n");
-   
-    image.open(RGB, ios::in | ios::binary); //Example image input: "sample_640x480.rgb"
-    output_image.open(YUV, ios::out | ios::binary); //Example image output: "sample_640x480.yuv"
+    in_image.open(input_image, ios::in | ios::binary); //Example image input: "sample_640x480.rgb"
+    out_image.open(output_image, ios::out | ios::binary); //Example image output: "sample_639x480.yuv"
 
     int i = 0;
     char buf_rgb[3];
     char* buf_yuv;
 
-    while (image.read(buf_rgb, sizeof(buf_rgb))) {
-        buf_yuv = convert(buf_rgb);
+    while (in_image.read(buf_rgb, sizeof(buf_rgb))) {
+        buf_yuv = convert_pixel(buf_rgb);
 	
-	memcpy(&pixels_rgb[i*3], buf_rgb, sizeof(buf_rgb));
+	//memcpy(&pixels_rgb[i*3], buf_rgb, sizeof(buf_rgb));
 	memcpy(&pixels_yuv[i*3], buf_yuv, sizeof(buf_rgb));
 
 	i++;
     }
 
-    image.close();
-    output_image.write(pixels_yuv, sizeof(pixels_yuv)); 
-    output_image.close();
+    in_image.close();
+    out_image.write(pixels_yuv, sizeof(pixels_yuv)); 
+    out_image.close();
+}
 
-    return 1;
+int main (int argc, char **argv) {
+
+    char *RGB_file_name;
+    char *YUV_file_name;
+
+    struct timeval time1;
+    struct timeval time2;
+    long int elapsed_time;
+
+    if (menu(argc,argv,&RGB_file_name,&YUV_file_name)){
+        return 1;
+    }
+
+    printf("Executing convertion!!\n");
+
+    gettimeofday(&time1, NULL);
+    rgb2yuv(RGB_file_name,YUV_file_name);
+    gettimeofday(&time2, NULL);
+
+    elapsed_time = time2.tv_usec - time1.tv_usec;
+
+    printf("Elapsed time: %ld us\n", elapsed_time);
+
+    return 0;
 }
